e50b271de7a62025694392a95641644503958ef1
db12354 <c24373171@mytudublin.ie>
2024-12-03 17:43:25 +0000

    Add files via upload

--- JAVASCRIPT/main.js
+++ JAVASCRIPT/main.js
@@ -1,13 +1,30 @@
+/*
 function validateForm() {
   // Get the value of the input field with id="numb"
+  
   let x = document.getElementById("phone").value;
   // If x is Not a Number or less than one or greater than 10
   let text;
   // The || symbol means OR
-  if (isNaN(x) || x < 1 || x > 10) {
+  if(x.length !== 10 && isNaN(x)){
     text = "Invalid phone number!";
   } else {
     text = "input sent!";
   }
   document.getElementById("submit").innerHTML = text;
 }
+  this didnt work so i decided it would be easier to do a different one 
+*/
+
+function validateForm() {
+  let x = document.forms["myForm"]["first"].value;
+  let y = document.forms["myForm"]["second"].value;
+  if (x && y !== '') {
+    alert("Submitted succesfully, feel free to send another.");
+    return true;
+  }
+  else{
+    alert("Please fill in all of the boxes!");
+    return false;
+  }
+} 
